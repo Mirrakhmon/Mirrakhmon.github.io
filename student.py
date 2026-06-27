4a7b3995b9481ce18eafa2eba4d282e7f5ac21aa
Mirrakhmon <45327177+Mirrakhmon@users.noreply.github.com>
2026-06-28 00:36:53 +0500

    Enhance Students class to include patronus attribute and charm method

--- student.py
+++ student.py
@@ -1,24 +1,38 @@
 class Students:
-    def __init__(self, name, house):
-        if not name :
-            raise ValueError("Missing name or")
+    def __init__(self, name, house, patronus=None):
+        if not name:
+            raise ValueError("Missing name")
         if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
             raise ValueError("Invalid house")
         self.name = name
         self.house = house
+        self.patronus = patronus
+
     def __str__(self):
-        return f"{self.name} from {self.house}"
+        return f"{self.name} from {self.house} has a {self.patronus} patronus."
+
+    def charm(self):
+        match self.patronus.lower():
+            case "stag":
+                return "Expecto stag!"
+            case "otter":
+                return "Expecto otter!"
+            case "jack russell terrier":
+                return "Expecto terrier!"
+            case _:
+                return "Expecto patronum!"
 
 def main():
     student = get_student()
     if student:
-        print(f"{student.name} from {student.house}.")
+        print(student.charm())
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
+    patronus = input("Patronus: ")
     try:
-        return Students(name, house)
+        return Students(name, house, patronus)
     except ValueError as e:
         print(e)
         return None
