bf3b2232596a062cc8647fc3c73cd3ebc09e55bf
Mirrakhmon <45327177+Mirrakhmon@users.noreply.github.com>
2026-07-27 19:22:18 +0500

    solve problem 23: reverse a string

---
+++ python-50-problems/reverse_a_string.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+class StringReverser:
+    def __init__(self,text):
+        self.text=text
+    def __str__(self):
+        return f"{self.reverse()}"
+    def reverse(self):
+        text=self.text
+        rtext=""
+        for i in range(len(text)):
+            rtext+=text[-i-1]
+        return rtext
+print(f"=== Run: {datetime.now().strftime('%d.%m.%Y %H:%M:%S')} ===")
+print(StringReverser("hello"))    # olleh
+print(StringReverser("a"))         # a          <- граница: один символ
+print(StringReverser(""))           # (пустая строка)  <- граница: пустой ввод
+print(StringReverser("racecar"))   # racecar    <- палиндром, развернётся сам в себя
